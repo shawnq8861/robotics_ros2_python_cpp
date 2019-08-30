55eba843b3ef33f06e4ecc92d07216e8983e0c18
shawnq8861 <shawnquinn861@gmail.com>
2019-08-30 15:30:44 -0700

    added arg arror checking

--- non_ros2_src/set_cmd_vel.py
+++ non_ros2_src/set_cmd_vel.py
@@ -3,10 +3,37 @@
 import subprocess
 import sys
 
+#
+# pass in a single command line argment of 4 characters
+# examples:
+# python3 set_cmd_vel.py 0000
+# python3 set_cmd_vel.py v025
+# python3 set_cmd_vel.py m001
+# python3 set_cmd_vel.py m000
+# python3 set_cmd_vel.py m101
+# python3 set_cmd_vel.py m100
+#
+
 def main():
     #
-    # build the command string
-    # use arg passed in for speed
+    # error checking follows:
+    #
+    # must have one argument
+    #
+    usage_str = "usage:\npython3 set_cmd_vel.py xxxx"
+    arg_count = len(sys.argv)
+    if (arg_count != 2):
+    	print(usage_str)
+    	return
+    #
+    # argument must be 4 chars long
+    #
+    arg_str = str(sys.argv[1])
+    if (len(arg_str) != 4):
+    	print(usage_str)
+    	return
+    #
+    # all good, build command string and send it
     #
     cmd_str = "echo "
     cmd_str = cmd_str + str(sys.argv[1])
