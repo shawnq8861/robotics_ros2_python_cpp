9eb37c37aaac49c83b4b5f3bbdfc5e31c2f7ecc7
shawnq8861 <shawnquinn861@gmail.com>
2021-11-14 12:10:10 -0800

    clean up to remove link errors

--- qbot_nodes_cpp/src/base_controller.cpp
+++ qbot_nodes_cpp/src/base_controller.cpp
@@ -1,92 +1,34 @@
-// Copyright 2014 Open Source Robotics Foundation, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-#include <cstdio>
 #include <memory>
-#include <string>
 
 #include "rclcpp/rclcpp.hpp"
-#include "rcutils/cmdline_parser.h"
-
-#include "std_msgs/msg/string.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 
-void print_usage()
-{
-  printf("Usage for base_controller app:\n");
-  printf("base_controller [-t topic_name] [-h]\n");
-  printf("options:\n");
-  printf("-h : Print this help function.\n");
-  printf("-t topic_name : Specify the topic on which to subscribe. Defaults to chatter.\n");
-}
+using std::placeholders::_1;
 
-// Create a Listener class that subclasses the generic rclcpp::Node base class.
-// The main function below will instantiate the class as a ROS node.
 class BaseController : public rclcpp::Node
 {
 public:
-  explicit BaseController(const std::string & cmd_vel_topic)
-  : Node("base_controller")
-  {
-    // Create a callback function for when messages are received.
-    // Variations of this function also exist using, for example UniquePtr for zero-copy transport.
-    auto cmdVelCallback =
-      [this](const geometry_msgs::msg::Twist::SharedPtr msg) -> void
-      {
-        RCLCPP_INFO(this->get_logger(), "I heard: [%f]", msg->linear.x);
-      };
-
-    // Create a subscription to the topic which can be matched with one or more compatible ROS
-    // publishers.
-    // Note that not all publishers on the same topic with the same type will be compatible:
-    // they must have compatible Quality of Service policies.
-    sub_ = create_subscription<geometry_msgs::msg::Twist>(cmd_vel_topic, 10, cmdVelCallback);
-  }
+    BaseController()
+    : Node("base_controller")
+    {
+        subscription_ = this->create_subscription<geometry_msgs::msg::Twist>(
+        "cmd_vel", 10, std::bind(&BaseController::cmd_vel_callback, this, _1));
+    }
 
 private:
-  rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr sub_;
+    void cmd_vel_callback(const geometry_msgs::msg::Twist::SharedPtr msg)
+    {
+      RCLCPP_INFO(this->get_logger(), "I heard: [%f]", msg->linear.x);
+    }
+    rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr subscription_;
 };
 
 int main(int argc, char * argv[])
 {
-  // Force flush of the stdout buffer.
-  setvbuf(stdout, NULL, _IONBF, BUFSIZ);
-
-  if (rcutils_cli_option_exist(argv, argv + argc, "-h")) {
-    print_usage();
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<BaseController>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    
     return 0;
-  }
-
-  // Initialize any global resources needed by the middleware and the client library.
-  // You must call this before using any other part of the ROS system.
-  // This should be called once per process.
-  rclcpp::init(argc, argv);
-
-  // Parse the command line options.
-  auto topic = std::string("cmd_vel");
-  char * cli_option = rcutils_cli_get_option(argv, argv + argc, "-t");
-  if (nullptr != cli_option) {
-    topic = std::string(cli_option);
-  }
-
-  // Create a node.
-  auto node = std::make_shared<BaseController>(topic);
-
-  // spin will block until work comes in, execute work as it becomes available, and keep blocking.
-  // It will only be interrupted by Ctrl-C.
-  rclcpp::spin(node);
-
-  rclcpp::shutdown();
-  return 0;
 }
