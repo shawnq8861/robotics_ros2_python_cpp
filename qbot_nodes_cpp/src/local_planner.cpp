2876b3dbf404fe8b5cbaadcdc6068e98f465c5bf
shawnq8861 <shawnquinn861@gmail.com>
2021-11-15 12:14:48 -0800

    completed testing heading speed service call

--- qbot_nodes_cpp/src/local_planner.cpp
+++ qbot_nodes_cpp/src/local_planner.cpp
@@ -30,17 +30,18 @@ public:
         //
         // use lambda function to handle service callback
         //
+        // command to test:  ros2 service call /set_heading_speed 
+        // qbot_nodes_cpp/srv/HeadingSpeed "{heading: 3.24, speed: 12.9}"
+        //
         auto handle_set_heading_speed =
             [this](const std::shared_ptr<rmw_request_id_t> request_header,
                 const std::shared_ptr<qbot_nodes_cpp::srv::HeadingSpeed::Request> request,
                             std::shared_ptr<qbot_nodes_cpp::srv::HeadingSpeed::Response> response) -> void
         {
             (void)request_header;
-            //RCLCPP_INFO(
-            //this->get_logger(), "Incoming request\na: %" PRId64 " b: %" PRId64,
-            //request->a, request->b);
             heading_ = request->heading;
             speed_ = request->speed;
+            RCLCPP_INFO(this->get_logger(), "Requested heading: '%f', speed: '%f'", heading_, speed_);
             response = 0;
         };
         service_ = create_service<qbot_nodes_cpp::srv::HeadingSpeed>("set_heading_speed", handle_set_heading_speed);
