028a603205a86fc7fd37fdd8b0261133934acdc0
shawnq8861 <shawnquinn861@gmail.com>
2021-11-14 20:20:26 -0800

    added service call to local planner

--- qbot_nodes_cpp/src/local_planner.cpp
+++ qbot_nodes_cpp/src/local_planner.cpp
@@ -5,6 +5,7 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/twist.hpp"
+#include "qbot_nodes_cpp/srv/heading_speed.hpp"
 
 using namespace std::chrono_literals;
 
@@ -13,37 +14,60 @@ using namespace std::chrono_literals;
 
 class LocalPlanner : public rclcpp::Node
 {
-  public:
+public:
     LocalPlanner()
     : Node("local_planner")
     {
-      publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
-      timer_ = this->create_wall_timer(
-      500ms, std::bind(&LocalPlanner::timer_callback, this));
-      cmd_vel_msg.linear.y = 0.0;
-      cmd_vel_msg.linear.z = 0.0;
-      cmd_vel_msg.angular.x = 0.0;
-      cmd_vel_msg.angular.y = 0.0;
+        publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
+        timer_ = this->create_wall_timer(
+            500ms, std::bind(&LocalPlanner::timer_callback, this));
+        cmd_vel_msg.linear.y = 0.0;
+        cmd_vel_msg.linear.z = 0.0;
+        cmd_vel_msg.angular.x = 0.0;
+        cmd_vel_msg.angular.y = 0.0;
+        heading_ = 0.0;
+        speed_ = 0.0;
+        //
+        // use lambda function to handle service callback
+        //
+        auto handle_set_heading_speed =
+            [this](const std::shared_ptr<rmw_request_id_t> request_header,
+                const std::shared_ptr<qbot_nodes_cpp::srv::HeadingSpeed::Request> request,
+                            std::shared_ptr<qbot_nodes_cpp::srv::HeadingSpeed::Response> response) -> void
+        {
+            (void)request_header;
+            //RCLCPP_INFO(
+            //this->get_logger(), "Incoming request\na: %" PRId64 " b: %" PRId64,
+            //request->a, request->b);
+            heading_ = request->heading;
+            speed_ = request->speed;
+            response = 0;
+        };
+        service_ = create_service<qbot_nodes_cpp::srv::HeadingSpeed>("set_heading_speed", handle_set_heading_speed);
     }
 
-  private:
+private:
     void timer_callback()
     {
-      cmd_vel_msg.linear.x = 1.37;
-      RCLCPP_INFO(this->get_logger(), "Publishing linear x: '%f'", cmd_vel_msg.linear.x);
-      cmd_vel_msg.angular.z = .259;
-      RCLCPP_INFO(this->get_logger(), "Publishing angular z: '%f'", cmd_vel_msg.angular.z);
-      publisher_->publish(cmd_vel_msg);
+        cmd_vel_msg.linear.x = 1.37;
+        RCLCPP_INFO(this->get_logger(), "Publishing linear x: '%f'", cmd_vel_msg.linear.x);
+        cmd_vel_msg.angular.z = .259;
+        RCLCPP_INFO(this->get_logger(), "Publishing angular z: '%f'", cmd_vel_msg.angular.z);
+        publisher_->publish(cmd_vel_msg);
     }
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
     geometry_msgs::msg::Twist cmd_vel_msg;
+    rclcpp::Service<qbot_nodes_cpp::srv::HeadingSpeed>::SharedPtr service_;
+    double heading_;
+    double speed_;
 };
 
 int main(int argc, char * argv[])
 {
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<LocalPlanner>());
-  rclcpp::shutdown();
-  return 0;
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<LocalPlanner>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
 }
