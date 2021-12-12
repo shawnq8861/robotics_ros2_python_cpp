c0f6ff6438b039cb89a1d040c9c18f3368b57fd7
shawnq8861 <shawnquinn861@gmail.com>
2021-12-11 21:07:53 -0800

    added roboclaw python library

---
+++ non_ros2_src/motor1_forward.py
@@ -0,0 +1,59 @@
+#!/usr/local/bin/python3
+#
+# to run:
+# 
+#
+import sys
+from roboclaw import Roboclaw
+import time
+
+def main():
+    #
+    # error checking follows:
+    #
+    # must have one argument
+    #
+    usage_str = "usage:\npython3 motor_forward.py XXX"
+    arg_count = len(sys.argv)
+    if (arg_count != 2):
+    	print(usage_str)
+    #
+    # get the move speed, must be between 0 and 127
+    #
+    else:
+        counts = int(sys.argv[1])
+        #
+        # address of the RoboClaw as set in Motion Studio
+        #
+        address = 0x80
+        #
+        # Creating the RoboClaw object, serial port and baudrate passed
+        #
+        robo = Roboclaw("/dev/ttymxc2", 9600) 
+        #
+        # Starting communication with the RoboClaw hardware
+        #
+        opened = robo.Open()
+        if opened:
+            #
+            # Start motor 1 in the forward direction at half speed
+            #
+            robo.ForwardM1(address, counts)
+            #
+            # pause for three seconds
+            #
+            time.sleep(3.0)
+            #
+            # stop the motor
+            #
+            robo.ForwardM1(address, 0)
+        else:
+            print("port did not open")
+
+    return
+
+if __name__ == "__main__":
+        #
+        # run main as standalone applicatiuon
+        #
+    main()
---
+++ non_ros2_src/motor1_position.py
@@ -0,0 +1,41 @@
+#!/usr/local/bin/python3
+#
+# to run:
+# 
+#
+import sys
+from roboclaw import Roboclaw
+import time
+
+def main():
+    #
+    # get the encoder counts
+    #
+    # address of the RoboClaw as set in Motion Studio
+    #
+    address = 0x80
+    #
+    # Creating the RoboClaw object, serial port and baudrate passed
+    #
+    robo = Roboclaw("/dev/ttymxc2", 9600) 
+    #
+    # Starting communication with the RoboClaw hardware
+    #
+    opened = robo.Open()
+    if opened:
+        #
+        # Start motor 1 in the forward direction at half speed
+        #
+        counts = robo.ReadEncM1(address)
+        print("motor 1 counts: ", counts)
+        time.sleep(1)
+    else:
+        print("port did not open")
+
+    return
+
+if __name__ == "__main__":
+        #
+        # run main as standalone applicatiuon
+        #
+    main()
