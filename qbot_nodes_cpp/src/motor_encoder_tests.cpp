253538ba53806eabe0c34810076519a99fdecf4a
shawnq8861 <shawnquinn861@gmail.com>
2021-12-18 16:03:41 -0800

    debug motor speed setting function call

--- qbot_nodes_cpp/src/motor_encoder_tests.cpp
+++ qbot_nodes_cpp/src/motor_encoder_tests.cpp
@@ -102,12 +102,12 @@ int main(int argc, char * argv[])
             RCLCPP_INFO_STREAM(motor_encoder_test_node->get_logger(), "set motor speed successfully...\n");
         }
         std::this_thread::sleep_for(2s);
-        speed_m1 = 0;
-        speed_m2 = 0;
+        //speed_m1 = 0;
+        //speed_m2 = 0;
         //
         // stop the motors
         //
-        if (roboclaw_speed_m1m2(robo, address, speed_m1, speed_m2) != ROBOCLAW_OK) {
+        if (roboclaw_speed_m1m2(robo, address, 0, 0) != ROBOCLAW_OK) {
             RCLCPP_INFO_STREAM(motor_encoder_test_node->get_logger(), "could not set motor speed...\n");
         }
         else {
